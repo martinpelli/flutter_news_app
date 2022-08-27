e7da18c75ee81903823bff3d02e7951a4ce611af
Martin Pellicer <martinpelli@outlook.com>
2022-08-27 16:19:33 -0300

    Main page

--- lib/src/pages/tabs_page.dart
+++ lib/src/pages/tabs_page.dart
@@ -3,9 +3,36 @@ import 'package:flutter/material.dart';
 class TabsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: PageView(
-      physics: BouncingScrollPhysics(),
+    return Scaffold(body: _Pages(), bottomNavigationBar: _NavigationBar());
+  }
+}
+
+class _NavigationBar extends StatelessWidget {
+  const _NavigationBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: 0,
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.person_outline), label: "For you"),
+        BottomNavigationBarItem(icon: Icon(Icons.public), label: "Headers")
+      ],
+    );
+  }
+}
+
+class _Pages extends StatelessWidget {
+  const _Pages({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      physics: const NeverScrollableScrollPhysics(),
       children: <Widget>[
         Container(
           color: Colors.red,
@@ -14,6 +41,6 @@ class TabsPage extends StatelessWidget {
           color: Colors.green,
         ),
       ],
-    ));
+    );
   }
 }
